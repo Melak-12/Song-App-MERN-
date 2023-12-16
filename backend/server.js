eec24db83423b9a3e3efcc535f2da5ce62f33888
melak-12 <melakabebeee@gmail.com>
2023-12-16 14:04:25 +0300

    cores

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,25 @@
-const path= require('path')
+const path = require('path');
 const express = require('express');
-const dotenv =require('dotenv').config();
-const colors =require('colors')
-const connectDB=require('./config/db')
-const port = process.env.PORT || 5000
-// const {errorHandler}=require('./middleware/errorMiddleware')
-//insert cores
-const cors=require('cors')
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
 connectDB();
-const app =express();
+const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended:false}));
+app.use(cors({ origin: 'https://songapp-ten.vercel.app' }));
 
-app.use('/api/songs',require('./routes/songRoutes'))
-app.use('/api/users',require('./routes/userRoutes'))
+app.use('/api/songs', require('./routes/songRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
-//serve frontend
-/*
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'./build')))
+app.get('/', (req, res) => {
+    res.send('welcome to songs app  :)');
+});
 
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'./','build','index.html')))
-}
-else{
-    app.get('/' , (req , res)=>{
-    
-       res.send('set to prodctioin :)')
-    
-    })
-}
-
-*/
-//try to accept client on other
-app.get('/' , (req , res)=>{
-    
-    res.send('welcome to songs app  :)')
- 
- })
-
-app.use(cors({ origin:'https://songapp-ten.vercel.app' }));
-
-
-console.log("ready for frontend")
-// app.use(errorHandler)
-app.listen(port,()=>console.log(`server started at port ${port}`.bgGreen.white))
+console.log("ready for frontend");
+app.listen(port, () => console.log(`server started at port ${port}`.bgGreen.white));
